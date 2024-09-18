2887a3772c9483b1ce139d88fc77fc79035e8945
Ambryal <85936037+Ambryal@users.noreply.github.com>
2024-09-18 16:19:24 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,28 +1,72 @@
-function cl(a){if(arguments.length!=1) a=[...arguments];console.log(a)}
 
-function addScript( src ) {
-	cl("addscript",src);
-	fetch(src)
+
+
+
+fetch(window.location.href)
+  .then(response => response.text())
+  .then(html => {
+    var codeSource = html; 
+	
+	
+	var pos = '<script defer="defer" src="';
+	var jsfile = "/static/js/main.";
+	var end = '.js">';
+	var num = codeSource.split(pos+jsfile);
+	if(num.length != 2){cl("trouve pas le .js",pos+jsfile);return}
+	
+	mid = num[1].split(end)[0];
+	
+	cl("fetching : ",jsfile+mid+end);
+	fetch(jsfile+mid+".js")
 	  .then(response => response.text())
-	  .then(html => {
-	  
-		  var s = document.createElement( 'script' );
-		  s.setAttribute('text',"text/javascript");
-		  s.setAttribute('type',"module");
-		  s.textContent=html;
-		  document.body.appendChild( s );
+	  .then(html2 => {
+	  		
+		var js = html2;
+		
+		
+		
+		var colorflag = ".getRandomColors)";
+		var newColor = "GET_RANDOM_COLOR)";
+		
+		js=js.split(colorflag);
+		
+		for(var i in js){
+			if(i==js.length-1)break;
+			while(!js[i].endsWith(",")){js[i]=js[i].substring(0, js[i].length - 1);}
+		}
+
+		js = js.join(newColor);
+		
+		fetch(BRUTALISATOR+"color.js")
+		  .then(response => response.text())
+		  .then(color => {
+			codeSource = codeSource.split(pos+jsfile+mid+end);
+			codeSource = codeSource[0]+'<script type="module">'+color+js+"\nconsole.log('FIN');"+codeSource[1];
+			  
+			var iframe = document.createElement('iframe');
+
+			iframe.src = 'about:blank'; 
+
+			document.body.appendChild(iframe);
+
+			var iframeDoc = iframe.contentWindow.document;
+
+			iframeDoc.open();
+			
+			iframeDoc.write(codeSource);
+			iframeDoc.close();
+			cl(iframe);
+		  })
+		
+
 	  
 	  })
+	
+	
 
-	}
-
-
-const BRUTALISATOR = "https://raw.githubusercontent.com/Ambryal/BRUTALISATOR/main/"
-
-var url = window.location.href.split("/")
-url.shift()
-url.shift()
-if(url[url.length-1] == ""){url.pop();}
-
-if(url.length==1){addScript(BRUTALISATOR+"custom.js")}
-
+	
+	
+  })
+  .catch(error => {
+    console.error('Erreur:', error);
+  });
