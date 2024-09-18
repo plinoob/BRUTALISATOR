ba72c7030f7c89164534ea9121265ad2c88647ca
Ambryal <85936037+Ambryal@users.noreply.github.com>
2024-09-18 15:32:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 function cl(a){if(arguments.length!=1) a=[...arguments];console.log(a)}
 
-cl("mdrrrrr")
+function addScript( src ) {
+
+	fetch(src)
+	  .then(response => response.text())
+	  .then(html => {
+	  
+		  var s = document.createElement( 'script' );
+		  s.setAttribute('text',"text/javascript");
+		  s.setAttribute('type',"module");
+		  s.textContent=html;
+		  document.body.appendChild( s );
+	  
+	  })
+
+	}
+
+
+const BRUTALISATOR = "https://raw.githubusercontent.com/Ambryal/BRUTALISATOR/539bcc944bc4332b71987f14ba1e3393ffe2fa59/"
+
+var url = window.location.href.split("/")
+
+
+if(url.length==1){addScript(BRUTALISATOR+"custom.js")}
+
