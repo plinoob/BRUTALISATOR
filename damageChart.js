8efc7daaf75ff7ebe4e8645ccebc5be07844162a
Ambryal <85936037+Ambryal@users.noreply.github.com>
2024-09-19 09:14:33 +0200

    Add files via upload

---
+++ damageChart.js
@@ -0,0 +1,30 @@
+function cl(a){if(arguments.length!=1) a=[...arguments];console.log(a)}
+
+function addScript( src ) {
+	cl("addscript",src);
+	fetch(src)
+	  .then(response => response.text())
+	  .then(html => {
+	  
+		  var s = document.createElement( 'script' );
+		  s.setAttribute('text',"text/javascript");
+		  s.setAttribute('type',"module");
+		  s.textContent=html;
+		  document.body.appendChild( s );
+	  
+	  })
+
+	}
+
+
+const BRUTALISATOR = "https://raw.githubusercontent.com/Ambryal/BRUTALISATOR/main/";
+
+// Récupérer l'élément avec l'attribut aria-label="Afficher/masquer les logs"
+const element = document.querySelector('[aria-label="Afficher/masquer les logs"]');
+
+// Vérifier si l'élément existe, puis simuler un clic
+if (element) {
+  element.click();
+} else {
+  console.log("Élément non trouvé");
+}
