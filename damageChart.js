9e41dea06508fbc415aa821db4d52d149fba2961
Ambryal <gohieralexis@yahoo.fr>
2024-09-19 12:12:11 +0200

    Ajout des fichiers .js du dossier dist

--- damageChart.js
+++ damageChart.js
@@ -48,16 +48,16 @@ if (element) {
 
 function analyzeText(text){
 	
-	names_with_num = ["Armiv1","Armiv2","MGE-2Gaul"]
+	var names_with_num = ["Armiv1","Armiv2","MGE-2Gaul"]
 	
-	chart = {}
-	t1 = {}
-	t2 = {}
+	var chart = {}
+	var t1 = {}
+	var t2 = {}
 	
 	var lines=text.split("\n")
-	for(l of lines){
+	for(var l of lines){
 		
-		brute = l.split(" est arrivé !")
+		var brute = l.split(" est arrivé !")
 		if(brute.length>1){
 			var nom = brute[0]
 			if(parseInt(nom).toString() == nom) nom = " "+nom;
@@ -66,45 +66,45 @@ function analyzeText(text){
 		
 		
 		
-		choc =  l.split(" a fait tomber ")
+		var choc =  l.split(" a fait tomber ")
 		if(choc.length>1){
-			roxeur = choc[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
+			var roxeur = choc[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
 			//console.log("choc "+dmg)
 			chart[roxeur] = (chart[roxeur] || 0) + 0.001
 			continue
 		}
 		
-		vol =  l.split(" a volé ")
+		var vol =  l.split(" a volé ")
 		if(vol.length>1){
-			roxeur = vol[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
+			var roxeur = vol[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
 			//console.log("choc "+dmg)
 			chart[roxeur] = (chart[roxeur] || 0) + 0.001
 			continue
 		}
 		
-		vamp =  l.split(" a sucé le sang ")
+		var vamp =  l.split(" a sucé le sang ")
 		if(vamp.length>1){
-			dmg=parseInt(vamp[1].split(" infligeant ")[1].split(" ")[0])
-			roxeur = vamp[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
+			var dmg=parseInt(vamp[1].split(" infligeant ")[1].split(" ")[0])
+			var roxeur = vamp[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
 			console.log("vamp "+dmg)
 			chart[roxeur] = (chart[roxeur] || 0) + dmg
 			continue
 		}
 		
-		bomb =  l.split(" dégâts avec une bombe")
+		var bomb =  l.split(" dégâts avec une bombe")
 		if(bomb.length>1){
-			bomb = bomb[0].split(" a infligé ")
-			dmg=bomb[1].split(", ").reduce((acc, val) => acc + parseInt(val, 10), 0);
-			roxeur = bomb[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
+			var bomb = bomb[0].split(" a infligé ")
+			var dmg=bomb[1].split(", ").reduce((acc, val) => acc + parseInt(val, 10), 0);
+			var roxeur = bomb[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
 			console.log("bomb "+dmg)
 			chart[roxeur] = (chart[roxeur] || 0) + dmg
 			continue
 		}
 		
-		rox = l.split(" a infligé ")
+		var rox = l.split(" a infligé ")
 		if(rox.length>1){
-			dmg=parseInt(rox[1].split(" ")[0])
-			roxeur = rox[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
+			var dmg=parseInt(rox[1].split(" ")[0])
+			var roxeur = rox[0];if(parseInt(roxeur).toString() == roxeur) roxeur = " "+roxeur;
 			console.log("_ "+dmg)
 			chart[roxeur] = (chart[roxeur] || 0) + dmg
 			continue
@@ -112,13 +112,13 @@ function analyzeText(text){
 		if(/\d/.test(names_with_num.reduce((acc, name) => acc.replace(new RegExp(name, 'g'), ''), l))) console.log("SUS "+l)
 	}
 	
-	for(i of [t1,t2]) {
-		for(j in i) i[j] = chart[j] || 0
-		sorted = Object.entries(i).sort(([, a], [, b]) => b-a).reduce((result, [key, value]) => {
+	for(var i of [t1,t2]) {
+		for(var j in i) i[j] = chart[j] || 0
+		var sorted = Object.entries(i).sort(([, a], [, b]) => b-a).reduce((result, [key, value]) => {
   result[key] = value;
   return result;
 }, {})
-		for(i in sorted) console.log(i,parseInt(sorted[i]),
+		for(var i in sorted) console.log(i,parseInt(sorted[i]),
 		(Math.round((sorted[i]-parseInt(sorted[i])) * 1000)==0)?"":"  ("+Math.round((sorted[i]-parseInt(sorted[i])) * 1000)+" choc)")
 		console.log("")
 		}
