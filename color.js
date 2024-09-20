b9ffb15e1b88ab218bac6e1db5de5dafdbdf0353
Ambryal <gohieralexis@yahoo.fr>
2024-09-20 17:45:07 +0200

    Ajout des fichiers .js du dossier dist

--- color.js
+++ color.js
@@ -746,7 +746,7 @@ else{BODY = GENDER_BODY[gender];COLORS = GENDER_COLORS[gender]}};createTable()}
 
 function createCell(cell,i,j,l){
 	
-	//if(j>=l[3])return
+	if(j>=l[3])return
 	
 	$(cell).css({cursor:"pointer"})
 
