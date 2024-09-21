1b9217a9b6aea6c2b86618f87b6f766b043bd289
Ambryal <gohieralexis@yahoo.fr>
2024-09-21 07:54:32 +0200

    Ajout des fichiers .js du dossier dist

---
+++ fight.js
@@ -0,0 +1,710 @@
+var Gender = {
+  male: 'male',
+  female: 'female'
+};
+var pad = (n, width, z = '0') => {
+    var nString = `${n}`;
+    return nString.length >= width
+        ? nString
+        : new Array(width - nString.length + 1).join(z) + n.toString();
+};
+var readBodyString = (bodyString) => ({
+    p1: parseInt(bodyString[0] || '0', 16),
+    p1a: parseInt(bodyString[1] || '0', 16),
+    p1b: parseInt(bodyString[2] || '0', 16),
+    p2: parseInt(bodyString[3] || '0', 16),
+    p3: parseInt(bodyString[4] || '0', 16),
+    p4: parseInt(bodyString[5] || '0', 16),
+    p5: parseInt(bodyString[6] || '0', 16),
+    p6: parseInt(bodyString[7] || '0', 16),
+    p7: parseInt(bodyString[8] || '0', 16),
+    p7b: parseInt(bodyString[9] || '0', 16),
+    p8: parseInt(bodyString[10] || '0', 16),
+});
+var generateBodyString = (body) => [
+    body.p1.toString(16),
+    body.p1a.toString(16),
+    body.p1b.toString(16),
+    body.p2.toString(16),
+    body.p3.toString(16),
+    body.p4.toString(16),
+    body.p5.toString(16),
+    body.p6.toString(16),
+    body.p7.toString(16),
+    body.p7b.toString(16),
+    body.p8.toString(16),
+].join('');
+var getColor = (gender, part, color) => {
+    var skinParts = ['col0', 'col0a', 'col0c'];
+    var hairParts = ['col1', 'col1a', 'col1b', 'col1c', 'col1d'];
+    var clothingParts = ['col2', 'col2a', 'col2b', 'col3', 'col3b', 'col4', 'col4a', 'col4b'];
+    let colorArray = [];
+    if (skinParts.includes(part))
+        colorArray = colors[gender].skin;
+    else if (hairParts.includes(part))
+        colorArray = colors[gender].hair;
+    else if (clothingParts.includes(part))
+        colorArray = colors[gender].clothing;
+    var normalColor = colorArray[color];
+    if (normalColor)
+        return normalColor;
+    return colors.special[99 - color] || '#ffffff';
+};
+var readColorString = (gender, colorsString) => ({
+    col0: (0, getColor)(gender, 'col0', +colorsString.slice(0, 2)),
+    col0a: (0, getColor)(gender, 'col0a', +colorsString.slice(2, 4)),
+    col0c: (0, getColor)(gender, 'col0c', +colorsString.slice(4, 6)),
+    col1: (0, getColor)(gender, 'col1', +colorsString.slice(6, 8)),
+    col1a: (0, getColor)(gender, 'col1a', +colorsString.slice(8, 10)),
+    col1b: (0, getColor)(gender, 'col1b', +colorsString.slice(10, 12)),
+    col1c: (0, getColor)(gender, 'col1c', +colorsString.slice(12, 14)),
+    col1d: (0, getColor)(gender, 'col1d', +colorsString.slice(14, 16)),
+    col2: (0, getColor)(gender, 'col2', +colorsString.slice(16, 18)),
+    col2a: (0, getColor)(gender, 'col2a', +colorsString.slice(18, 20)),
+    col2b: (0, getColor)(gender, 'col2b', +colorsString.slice(20, 22)),
+    col3: (0, getColor)(gender, 'col3', +colorsString.slice(22, 24)),
+    col3b: (0, getColor)(gender, 'col3b', +colorsString.slice(24, 26)),
+    col4: (0, getColor)(gender, 'col4', +colorsString.slice(26, 28)),
+    col4a: (0, getColor)(gender, 'col4a', +colorsString.slice(28, 30)),
+    col4b: (0, getColor)(gender, 'col4b', +colorsString.slice(30, 32)),
+});
+var generateColorString = (colorObject) => [
+    (0, pad)(colorObject.col0, 2),
+    (0, pad)(colorObject.col0a, 2),
+    (0, pad)(colorObject.col0c, 2),
+    (0, pad)(colorObject.col1, 2),
+    (0, pad)(colorObject.col1a, 2),
+    (0, pad)(colorObject.col1b, 2),
+    (0, pad)(colorObject.col1c, 2),
+    (0, pad)(colorObject.col1d, 2),
+    (0, pad)(colorObject.col2, 2),
+    (0, pad)(colorObject.col2a, 2),
+    (0, pad)(colorObject.col2b, 2),
+    (0, pad)(colorObject.col3, 2),
+    (0, pad)(colorObject.col3b, 2),
+    (0, pad)(colorObject.col4, 2),
+    (0, pad)(colorObject.col4a, 2),
+    (0, pad)(colorObject.col4b, 2),
+].join('');
+var randomBetween = (min, max) => {
+    if (min > max)
+        return 0;
+    if (min === max)
+        return min;
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+var WeaponId;
+(function (WeaponId) {
+    WeaponId[WeaponId["fan"] = 0] = "fan";
+    WeaponId[WeaponId["keyboard"] = 1] = "keyboard";
+    WeaponId[WeaponId["knife"] = 2] = "knife";
+    WeaponId[WeaponId["leek"] = 3] = "leek";
+    WeaponId[WeaponId["mug"] = 4] = "mug";
+    WeaponId[WeaponId["sai"] = 5] = "sai";
+    WeaponId[WeaponId["racquet"] = 6] = "racquet";
+    WeaponId[WeaponId["axe"] = 7] = "axe";
+    WeaponId[WeaponId["bumps"] = 8] = "bumps";
+    WeaponId[WeaponId["flail"] = 9] = "flail";
+    WeaponId[WeaponId["fryingPan"] = 10] = "fryingPan";
+    WeaponId[WeaponId["hatchet"] = 11] = "hatchet";
+    WeaponId[WeaponId["mammothBone"] = 12] = "mammothBone";
+    WeaponId[WeaponId["morningStar"] = 13] = "morningStar";
+    WeaponId[WeaponId["trombone"] = 14] = "trombone";
+    WeaponId[WeaponId["baton"] = 15] = "baton";
+    WeaponId[WeaponId["halbard"] = 16] = "halbard";
+    WeaponId[WeaponId["lance"] = 17] = "lance";
+    WeaponId[WeaponId["trident"] = 18] = "trident";
+    WeaponId[WeaponId["whip"] = 19] = "whip";
+    WeaponId[WeaponId["noodleBowl"] = 20] = "noodleBowl";
+    WeaponId[WeaponId["piopio"] = 21] = "piopio";
+    WeaponId[WeaponId["shuriken"] = 22] = "shuriken";
+    WeaponId[WeaponId["broadsword"] = 23] = "broadsword";
+    WeaponId[WeaponId["scimitar"] = 24] = "scimitar";
+    WeaponId[WeaponId["sword"] = 25] = "sword";
+})(WeaponId || (exports.WeaponId = WeaponId = {}));
+var WeaponByName = {
+    [WeaponName.fan]: WeaponId.fan,
+    [WeaponName.keyboard]: WeaponId.keyboard,
+    [WeaponName.knife]: WeaponId.knife,
+    [WeaponName.leek]: WeaponId.leek,
+    [WeaponName.mug]: WeaponId.mug,
+    [WeaponName.sai]: WeaponId.sai,
+    [WeaponName.racquet]: WeaponId.racquet,
+    [WeaponName.axe]: WeaponId.axe,
+    [WeaponName.bumps]: WeaponId.bumps,
+    [WeaponName.flail]: WeaponId.flail,
+    [WeaponName.fryingPan]: WeaponId.fryingPan,
+    [WeaponName.hatchet]: WeaponId.hatchet,
+    [WeaponName.mammothBone]: WeaponId.mammothBone,
+    [WeaponName.morningStar]: WeaponId.morningStar,
+    [WeaponName.trombone]: WeaponId.trombone,
+    [WeaponName.baton]: WeaponId.baton,
+    [WeaponName.halbard]: WeaponId.halbard,
+    [WeaponName.lance]: WeaponId.lance,
+    [WeaponName.trident]: WeaponId.trident,
+    [WeaponName.whip]: WeaponId.whip,
+    [WeaponName.noodleBowl]: WeaponId.noodleBowl,
+    [WeaponName.piopio]: WeaponId.piopio,
+    [WeaponName.shuriken]: WeaponId.shuriken,
+    [WeaponName.broadsword]: WeaponId.broadsword,
+    [WeaponName.scimitar]: WeaponId.scimitar,
+    [WeaponName.sword]: WeaponId.sword,
+};
+var WeaponById = {
+    [WeaponId.fan]: WeaponName.fan,
+    [WeaponId.keyboard]: WeaponName.keyboard,
+    [WeaponId.knife]: WeaponName.knife,
+    [WeaponId.leek]: WeaponName.leek,
+    [WeaponId.mug]: WeaponName.mug,
+    [WeaponId.sai]: WeaponName.sai,
+    [WeaponId.racquet]: WeaponName.racquet,
+    [WeaponId.axe]: WeaponName.axe,
+    [WeaponId.bumps]: WeaponName.bumps,
+    [WeaponId.flail]: WeaponName.flail,
+    [WeaponId.fryingPan]: WeaponName.fryingPan,
+    [WeaponId.hatchet]: WeaponName.hatchet,
+    [WeaponId.mammothBone]: WeaponName.mammothBone,
+    [WeaponId.morningStar]: WeaponName.morningStar,
+    [WeaponId.trombone]: WeaponName.trombone,
+    [WeaponId.baton]: WeaponName.baton,
+    [WeaponId.halbard]: WeaponName.halbard,
+    [WeaponId.lance]: WeaponName.lance,
+    [WeaponId.trident]: WeaponName.trident,
+    [WeaponId.whip]: WeaponName.whip,
+    [WeaponId.noodleBowl]: WeaponName.noodleBowl,
+    [WeaponId.piopio]: WeaponName.piopio,
+    [WeaponId.shuriken]: WeaponName.shuriken,
+    [WeaponId.broadsword]: WeaponName.broadsword,
+    [WeaponId.scimitar]: WeaponName.scimitar,
+    [WeaponId.sword]: WeaponName.sword,
+};
+var WeaponType = {
+    FAST: 'fast',
+    SHARP: 'sharp',
+    HEAVY: 'heavy',
+    LONG: 'long',
+    THROWN: 'thrown',
+    BLUNT: 'blunt',
+};
+var WeaponTypeColor = {
+    fast: '#8686ff',
+    sharp: 'red',
+    heavy: 'brown',
+    long: 'green',
+    thrown: 'purple',
+    blunt: 'orange',
+};
+    'knife', 'broadsword', 'lance', 'baton', 'trident', 'hatchet',
+    'scimitar', 'axe', 'sword', 'fan', 'shuriken', 'bumps',
+    'morningStar', 'mammothBone', 'flail', 'whip',
+];
+var weapons = [
+    {
+        name: 'axe',
+        odds: 3,
+        types: ['heavy', 'blunt'],
+        tempo: 2.3,
+        reversal: -0.2,
+        evasion: -0.4,
+        dexterity: -0.3,
+        block: -0.5,
+        accuracy: 0.5,
+        disarm: 0.1,
+        combo: -0.4,
+        deflect: 0,
+        damage: 55,
+        toss: 5,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'baton',
+        odds: 70,
+        types: ['long'],
+        tempo: 1,
+        reversal: 0.3,
+        evasion: 0.05,
+        dexterity: 0,
+        block: 0.25,
+        accuracy: 0,
+        disarm: 0.25,
+        combo: 0.1,
+        deflect: 0,
+        damage: 6,
+        toss: 3,
+        reach: 3,
+        animation: 'estoc',
+    },
+    {
+        name: 'broadsword',
+        odds: 100,
+        types: ['sharp'],
+        tempo: 1.2,
+        reversal: 0.1,
+        evasion: 0,
+        dexterity: 0,
+        block: 0.15,
+        accuracy: 0,
+        disarm: 0.15,
+        combo: 0,
+        deflect: 0,
+        damage: 10,
+        toss: 5,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'bumps',
+        odds: 50,
+        types: ['heavy', 'blunt'],
+        tempo: 2,
+        reversal: -0.3,
+        evasion: -0.3,
+        dexterity: -0.35,
+        block: -0.3,
+        accuracy: 0.3,
+        disarm: 0.1,
+        combo: -0.6,
+        deflect: 0,
+        damage: 30,
+        toss: 5,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'fan',
+        odds: 2,
+        types: ['fast'],
+        tempo: 0.28,
+        reversal: 0.5,
+        evasion: 0.6,
+        dexterity: 0.5,
+        block: -0.5,
+        accuracy: 0,
+        disarm: -0.5,
+        combo: 0.45,
+        deflect: 0.25,
+        damage: 4,
+        toss: 5,
+        reach: 0,
+        animation: 'slash',
+    },
+    {
+        name: 'flail',
+        odds: 4,
+        types: ['heavy', 'blunt'],
+        tempo: 2.2,
+        reversal: 0,
+        evasion: -0.3,
+        dexterity: -0.1,
+        block: -0.5,
+        accuracy: 1.5,
+        disarm: -0.2,
+        combo: 0.3,
+        deflect: 0,
+        damage: 36,
+        toss: 5,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'fryingPan',
+        odds: 0.4,
+        types: ['heavy', 'blunt'],
+        tempo: 1.2,
+        reversal: 0,
+        evasion: 0,
+        dexterity: 0,
+        block: 0.4,
+        accuracy: 0,
+        disarm: 0,
+        combo: -0.4,
+        deflect: 0.4,
+        damage: 17,
+        toss: 2,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'halbard',
+        odds: 2,
+        types: ['long', 'sharp'],
+        tempo: 1.8,
+        reversal: 0,
+        evasion: 0,
+        dexterity: -0.4,
+        block: 0,
+        accuracy: 0,
+        disarm: 0.1,
+        combo: 0.1,
+        deflect: 0,
+        damage: 24,
+        toss: 2,
+        reach: 4,
+        animation: 'slash',
+    },
+    {
+        name: 'hatchet',
+        odds: 40,
+        types: ['heavy', 'sharp'],
+        tempo: 1.5,
+        reversal: 0,
+        evasion: 0,
+        dexterity: 0,
+        block: -0.1,
+        accuracy: 0,
+        disarm: 0,
+        combo: 0,
+        deflect: 0,
+        damage: 17,
+        toss: 3,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'keyboard',
+        odds: 0.4,
+        types: ['fast', 'blunt'],
+        tempo: 1,
+        reversal: 0,
+        evasion: 0.1,
+        dexterity: 0.2,
+        block: 0,
+        accuracy: 0,
+        disarm: 0,
+        combo: 0.5,
+        deflect: 0.3,
+        damage: 7,
+        toss: 2,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'knife',
+        odds: 80,
+        types: ['fast', 'sharp'],
+        tempo: 0.6,
+        reversal: 0,
+        evasion: 0.1,
+        dexterity: 0.5,
+        block: 0,
+        accuracy: 0,
+        disarm: 0,
+        combo: 0.3,
+        deflect: 0,
+        damage: 7,
+        toss: 5,
+        reach: 0,
+        animation: 'estoc',
+    },
+    {
+        name: 'lance',
+        odds: 40,
+        types: ['long'],
+        tempo: 1.2,
+        reversal: -0.1,
+        evasion: 0,
+        dexterity: 0,
+        block: 0,
+        accuracy: 0,
+        disarm: 0.1,
+        combo: 0,
+        deflect: 0,
+        damage: 12,
+        toss: 2,
+        reach: 3,
+        animation: 'estoc',
+    },
+    {
+        name: 'leek',
+        odds: 0.4,
+        types: ['fast', 'blunt'],
+        tempo: 1.1,
+        reversal: 1,
+        evasion: 0,
+        dexterity: 1,
+        block: -0.5,
+        accuracy: 2,
+        disarm: 0,
+        combo: 2,
+        deflect: 0,
+        damage: 5,
+        toss: 2,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'mammothBone',
+        odds: 20,
+        types: ['heavy', 'blunt'],
+        tempo: 1.6,
+        reversal: 0,
+        evasion: 0,
+        dexterity: 0,
+        block: 0,
+        accuracy: 0.5,
+        disarm: 0.1,
+        combo: -0.1,
+        deflect: 0,
+        damage: 14,
+        toss: 5,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'morningStar',
+        odds: 6,
+        types: ['heavy', 'blunt'],
+        tempo: 1.5,
+        reversal: 0,
+        evasion: -0.1,
+        block: 0,
+        accuracy: 0.3,
+        dexterity: -0.05,
+        disarm: 0.1,
+        combo: 0,
+        deflect: 0,
+        damage: 20,
+        toss: 5,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'mug',
+        odds: 0.4,
+        types: ['fast'],
+        tempo: 0.9,
+        reversal: 0,
+        evasion: 0.15,
+        dexterity: 0.3,
+        block: -0.1,
+        accuracy: 0,
+        disarm: 0,
+        combo: 0.4,
+        deflect: 0,
+        damage: 8,
+        toss: 2,
+        reach: 0,
+        animation: 'estoc',
+    },
+    {
+        name: 'noodleBowl',
+        odds: 0.4,
+        types: ['thrown'],
+        tempo: 0.45,
+        reversal: 0,
+        evasion: 0.1,
+        dexterity: 0,
+        block: -0.1,
+        accuracy: 0,
+        disarm: 0,
+        combo: 0.3,
+        deflect: 0,
+        damage: 10,
+        toss: 2,
+        reach: 0,
+        animation: 'fist',
+    },
+    {
+        name: 'piopio',
+        odds: 0.4,
+        types: ['thrown'],
+        tempo: 0.32,
+        reversal: 0,
+        evasion: 0.5,
+        dexterity: 0,
+        block: -0.1,
+        accuracy: 0,
+        disarm: 0.5,
+        combo: 0,
+        deflect: 0,
+        damage: 5,
+        toss: 2,
+        reach: 0,
+        animation: 'fist',
+    },
+    {
+        name: 'racquet',
+        odds: 0.4,
+        types: ['fast', 'blunt'],
+        tempo: 0.8,
+        reversal: 1,
+        evasion: 0.1,
+        dexterity: 0,
+        block: 0.2,
+        accuracy: 0,
+        disarm: 0,
+        combo: 0,
+        deflect: 0.5,
+        damage: 6,
+        toss: 2,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'sai',
+        odds: 6,
+        types: ['fast'],
+        tempo: 0.6,
+        reversal: 0,
+        evasion: 0.1,
+        dexterity: 0.25,
+        block: 0.3,
+        accuracy: 0,
+        disarm: 1,
+        combo: 0.3,
+        deflect: 0,
+        damage: 8,
+        toss: 5,
+        reach: 0,
+        animation: 'estoc',
+    },
+    {
+        name: 'scimitar',
+        odds: 6,
+        types: ['sharp'],
+        tempo: 0.8,
+        reversal: 0,
+        evasion: 0,
+        dexterity: 0.2,
+        block: 0.1,
+        accuracy: 0,
+        disarm: 0,
+        combo: 0.15,
+        deflect: 0,
+        damage: 10,
+        toss: 3,
+        reach: 1,
+        animation: 'slash',
+    },
+    {
+        name: 'shuriken',
+        odds: 8,
+        types: ['thrown'],
+        tempo: 0.12,
+        reversal: 0,
+        evasion: 0.15,
+        dexterity: 0,
+        block: -0.1,
+        accuracy: 0,
+        disarm: -0.5,
+        combo: 0.3,
+        deflect: 0,
+        damage: 3,
+        toss: 5,
+        reach: 0,
+        animation: 'fist',
+    },
+    {
+        name: 'sword',
+        odds: 4,
+        types: ['sharp'],
+        tempo: 1.8,
+        reversal: 0,
+        evasion: -0.2,
+        dexterity: -0.1,
+        block: 0,
+        accuracy: -0.2,
+        disarm: 0.1,
+        combo: 0,
+        deflect: 0,
+        damage: 28,
+        toss: 5,
+        reach: 2,
+        animation: 'slash',
+    },
+    {
+        name: 'trident',
+        odds: 10,
+        types: ['long'],
+        tempo: 1.4,
+        reversal: 0.05,
+        evasion: 0,
+        dexterity: 0,
+        block: 0,
+        accuracy: 0,
+        disarm: 0.2,
+        combo: 0,
+        deflect: 0,
+        damage: 14,
+        toss: 3,
+        reach: 3,
+        animation: 'estoc',
+    },
+    {
+        name: 'trombone',
+        odds: 0.4,
+        types: ['heavy', 'blunt'],
+        tempo: 2.5,
+        reversal: 0,
+        evasion: 0,
+        dexterity: -0.1,
+        block: 0.2,
+        accuracy: 0.2,
+        disarm: 0.5,
+        combo: 0.3,
+        deflect: 0,
+        damage: 20,
+        toss: 2,
+        reach: 2,
+        animation: 'slash',
+    },
+    {
+        name: 'whip',
+        odds: 3,
+        types: ['long'],
+        tempo: 1.5,
+        reversal: -0.1,
+        evasion: 0.3,
+        dexterity: 0.3,
+        block: -0.2,
+        accuracy: -0.2,
+        disarm: 0.3,
+        combo: 0.35,
+        deflect: 0,
+        damage: 10,
+        toss: 5,
+        reach: 5,
+        animation: 'whip',
+    },
+];
+var WEAPONS_SFX = {
+    ...weapons.reduce((acc, weapon) => {
+        acc[weapon.name] = [];
+        // Special SFX for some weapons
+        if (weapon.name === 'fryingPan') {
+            acc[weapon.name] = ['fryingPan'];
+            return acc;
+        }
+        if (weapon.name === 'lance') {
+            acc[weapon.name] = ['lance1', 'lance2'];
+            return acc;
+        }
+        if (weapon.name === 'piopio') {
+            acc[weapon.name] = ['piopio'];
+            return acc;
+        }
+        if (weapon.name === 'sword') {
+            acc[weapon.name] = ['sword'];
+            return acc;
+        }
+        if (weapon.name === 'trombone') {
+            acc[weapon.name] = ['trombone1', 'trombone2'];
+            return acc;
+        }
+        if (weapon.name === 'whip') {
+            acc[weapon.name] = ['whip'];
+            return acc;
+        }
+        // SFX depending on weapon type
+        if (weapon.types.includes('sharp')) {
+            acc[weapon.name].push('sharp1', 'sharp2', 'sharp3', 'sharp4', 'sharp5', 'sharp6', 'sharp7', 'sharp8');
+        }
+        else {
+            acc[weapon.name].push('blunt1', 'blunt2', 'blunt3', 'blunt4', 'blunt5', 'blunt6', 'blunt7', 'blunt8');
+        }
+        return acc;
+    }, {}),
+};
+"use strict"
+
