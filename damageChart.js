c5b6cd429116a1f31ff67938a10717e02c741360
Ambryal <gohieralexis@yahoo.fr>
2024-09-21 10:53:47 +0200

    Ajout des fichiers .js du dossier dist

--- damageChart.js
+++ damageChart.js
@@ -249,12 +249,13 @@ var sorted = Object.entries(detailedDamage[name]).sort(([, a], [, b]) => b-a).re
 }, {});
 var s=""
 for(var i in sorted){
-	s+=' <b class = "bruteNameHover">'+((i in weaponImages)?'<img height="20px" src="'+weaponImages[i]+'">':i)+'</b>   '+"\n"
+		s+='<font text-align="right" opacity="0.88" font-size="0.751429rem" color="rgb(140, 81, 64)"><b>'+sorted[i]+"</b></font>"+"\n"
+
 }
-if($('#floatingDiv').length>0){div({9:{"text-align":"right"},2:"span",17:s,0:$('#floatingDiv').empty()})}
+if($('#floatingDiv').length>0){div({9:{"text-align":"left"},2:"span",17:s,0:$('#floatingDiv').empty()})}
 s=""
 for(var i in sorted){
-	s+='<font text-align="left" opacity="0.88" font-size="0.751429rem" color="rgb(140, 81, 64)"><b>'+sorted[i]+"</b></font>"+"\n"
+	s+=' <b class = "bruteNameHover">'+((i in weaponImages)?'<img height="20px" src="'+weaponImages[i]+'">':i)+'</b>   '+"\n"
 }
 
 if($('#floatingDiv').length>0){div({2:"span",17:s,0:$('#floatingDiv')})}
