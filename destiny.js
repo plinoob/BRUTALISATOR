eb80b41a62fc0a7210dabcb98113640a82eff615
Ambryal <gohieralexis@yahoo.fr>
2024-09-21 17:37:26 +0200

    Ajout des fichiers .js du dossier dist

--- destiny.js
+++ destiny.js
@@ -1687,7 +1687,7 @@ function processNode(node, nodeId, nodes, edges, parentId = null) {
 		borderWidth = 2
 		borderColor = "#D87446"
 		 if (node.currentDestiny === true) {
-		borderColor = "#535D87"
+		borderColor = "#66719B"
 		} 
     }
 
