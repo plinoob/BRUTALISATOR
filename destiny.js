e14165382b5b9038b8a6baf2761e8149676b0633
Ambryal <gohieralexis@yahoo.fr>
2024-09-21 17:34:32 +0200

    Ajout des fichiers .js du dossier dist

--- destiny.js
+++ destiny.js
@@ -1652,7 +1652,7 @@ function processNode(node, nodeId, nodes, edges, parentId = null) {
 		},
 		pet:{
 			leaf:"#A7DD87",
-			node:"#C0C170",
+			node:"#D3D37C",
 		},
 		stats:{
 			leaf:"#DBC7B1",
