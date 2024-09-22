5114f5849c71cd09eb738d6636a6917ffdce4bc6
Ambryal <gohieralexis@yahoo.fr>
2024-09-22 22:08:04 +0200

    Ajout des fichiers .js du dossier dist

--- main.js
+++ main.js
@@ -2032,7 +2032,7 @@ var BossName = /*exports.*//*$Enums.*/BossName = {
 var VERSION = "v2.72.4"
 var ERROR_VERSION = "Mauvaise version de 💪BRUTALISATOR : "+VERSION+")"
 var ERROR_WEBSITE = "Utilise 💪BRUTALISATOR sur une page Labrute !"
-var NOTHING_TO_DO = "💪BRUTALISATOR n'a pas encore de fonctionnalité pour cette page ! \n\n============== FONCTIONNALITÉS ================\n\n• CRÉATION : Customisation de l'apparence, choix du maître\n\n• COMBAT : Récapitulatif des dommages\n\n• DESTINÉE : Meilleure visualisation"
+var NOTHING_TO_DO = "💪BRUTALISATOR n'a pas encore de fonctionnalité pour cette page ! \n\n============== FONCTIONNALITÉS ================\n\n• CRÉATION : Customisation de l'apparence, choix du maître\n\n• CLAN : Estimation des dégâts journaliers\n\n• COMBAT : Récapitulatif des dommages\n\n• DESTINÉE : Meilleure visualisation"
 function alertAndStop(error){stopLoading();alert(error);throw new Error(error);}
 
 if(!(window.location.href.startsWith("https://"+"b"+"rut"))){alertAndStop(ERROR_WEBSITE);}
