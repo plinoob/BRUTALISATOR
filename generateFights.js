aa355cea5e4689c08e18dce92741f6960869672e
Ambryal <gohieralexis@yahoo.fr>
2024-09-26 19:04:40 +0200

    Ajout des fichiers .js du dossier dist

--- generateFights.js
+++ generateFights.js
@@ -79,9 +79,9 @@ async function genBrute({
 	var not_boosterCount = brute.skills.reduce((acc, obj) => {if(not_boosters.includes(obj)){acc++};return acc;}, 0)
 	not_boosters.splice(not_boosters.indexOf("spy"), 1)
 	if(Math.random()*2>1){not_boosters.splice(not_boosters.indexOf("flashflood"), 1)};
-    if(Math.random()*3>1){not_boosters.splice(not_boosters.indexOf("monk"), 1)};
-    if(Math.random()*6>1){not_boosters.splice(not_boosters.indexOf("hideaway"), 1)};
-	if(Math.random()*6>1){not_boosters.splice(not_boosters.indexOf("chef"), 1)};
+    if(Math.random()*5>1){not_boosters.splice(not_boosters.indexOf("monk"), 1)};
+    if(Math.random()*8>1){not_boosters.splice(not_boosters.indexOf("hideaway"), 1)};
+	if(Math.random()*8>1){not_boosters.splice(not_boosters.indexOf("chef"), 1)};
 	brute.skills = keepBoosters.concat(shuffle(not_boosters).slice(0,not_boosterCount))
 	
 	}
