10f82f7f37d8dfd5ee4f6f7d686a3febee3578c5
Ambryal <gohieralexis@yahoo.fr>
2024-09-26 18:29:57 +0200

    Ajout des fichiers .js du dossier dist

--- color.js
+++ color.js
@@ -50,6 +50,7 @@ async function genBrute({
 	var keepBoosters = brute.skills.reduce((acc, obj) => {if(boosters.includes(obj)){acc.push(obj)};return acc;}, [])
 	var not_boosters = skills.reduce((acc, obj) => {if(obj.type!="booster"){acc.push(obj.name)};return acc;}, [])
 	var not_boosterCount = brute.skills.reduce((acc, obj) => {if(not_boosters.includes(obj)){acc++};return acc;}, 0)
+	not_boosters.splice(not_boosters.indexOf("spy"), 1)
 	if(Math.random()*2>1){not_boosters.splice(not_boosters.indexOf("flashflood"), 1)};
     if(Math.random()*3>1){not_boosters.splice(not_boosters.indexOf("monk"), 1)};
     if(Math.random()*6>1){not_boosters.splice(not_boosters.indexOf("hideaway"), 1)};
