35ee7307d7305a3720bd67a5315beff5468a2367
Ambryal <gohieralexis@yahoo.fr>
2024-09-27 20:00:05 +0200

    Ajout des fichiers .js du dossier dist

--- main.js
+++ main.js
@@ -3510,7 +3510,7 @@ var img_ours = "/images/arena/bear.webp"
 var VERSION = "v2.72.4"
 var ERROR_VERSION = "Mauvaise version de 💪BRUTALISATOR : "+VERSION+")"
 var ERROR_WEBSITE = "Utilise 💪BRUTALISATOR sur une page Labrute !"
-var NOTHING_TO_DO = "💪BRUTALISATOR n'a pas encore de fonctionnalité pour cette page ! \n============== FONCTIONNALITÉS ================\n\n• CRÉATION : Customisation de l'apparence, choix du maître\n• CLAN : Estimation des dégâts journaliers\n• COMBAT : Récapitulatif des dommages\n• VERSUS : Génération et partage de combats entre brutes fictives\n• DESTINÉE : Meilleure visualisation"
+var NOTHING_TO_DO = "💪BRUTALISATOR n'a pas encore de fonctionnalité pour cette page ! \n============== FONCTIONNALITÉS ================\n\n• CRÉATION : Customisation de l'apparence, choix du maître\n• CLAN : Estimation des dégâts journaliers\n• ARÈNE : Voyance\n• COMBAT : Récapitulatif des dommages\n• VERSUS : Génération et partage de combats entre brutes fictives\n• DESTINÉE : Meilleure visualisation"
 function alertAndStop(error){stopLoading();alert(error);throw new Error(error);}
 cl("START")
 if(!(window.location.href.startsWith("https://"+"b"+"rut"))){alertAndStop(ERROR_WEBSITE);}
