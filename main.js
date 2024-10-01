d1a1015b396a7f963235260ac4ce7d3ec3ae65be
Ambryal <gohieralexis@yahoo.fr>
2024-10-01 18:06:57 +0200

    Ajout des fichiers .js du dossier dist

--- main.js
+++ main.js
@@ -3647,7 +3647,7 @@ else if(url.length==1){/*
 	
 	//*/addScript(BRUTALISATOR+"custom.js")
 	}
-else if(url.length==4 && url[2]=="tournament"){BRUTE = url[1];lauchFightVersusChampion()}
+else if(url[2].startsWith("tournament")){BRUTE = url[1];lauchFightVersusChampion()}
 else if(url.length==4 && url[2]=="clan" && url[3]=="ranking"){BRUTE = url[1];window.location.href="/"+BRUTE+"/clan/4c007438-9aa0-419e-af73-d82f8dc39eae";}
 else if(url.length==3 && url[2]=="destiny"){BRUTE = url[1];addScript(BRUTALISATOR+"destiny.js")}
 else if(url.length==3 && url[2]=="arena" /*&& BRANCHE=="dev"*/){BRUTE = url[1];arena()}
