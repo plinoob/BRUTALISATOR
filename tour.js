d1f4ab134de380a0eb4cd0f0a3910c8ceddd88df
Ambryal <gohieralexis@yahoo.fr>
2024-10-07 07:59:14 +0200

    Ajout des fichiers .js du dossier dist

--- tour.js
+++ tour.js
@@ -2624,7 +2624,7 @@ function Hall(){clickedOnHall = false}
 function Versus(){clickedOnBestMatchup = false}
 function Fight(){clickedOnLaunchFight = false}
 function Tournoi(){clickedOnTournoi = false}
-cl("yyyyyyyyyyyyy")
+cl("eeeeeeeeeeeee")
 var actions={
 	Hall : {
 		End:[isEnded,endTour],
