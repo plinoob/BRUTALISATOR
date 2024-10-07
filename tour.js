86167aa33b53f020b6b62351d230f747901c2faa
Ambryal <gohieralexis@yahoo.fr>
2024-10-07 07:59:00 +0200

    Ajout des fichiers .js du dossier dist

--- tour.js
+++ tour.js
@@ -2608,7 +2608,7 @@ function clickOnHall(){var elem = findTextInDOM("Hall","span");if(elem && !click
 function clickOnDinoReward(){var elem = findTextInDOM("Eternal DinoRPG","button");if(elem){dinoReward=true;$(elem).click()}}
 function clickOnLaunchFight(){var elem = findTextInDOM("Lancer le combat","div");if(elem && !clickedOnLaunchFight){clickedOnLaunchFight=true;$(elem).click()}}
 function clickOnCell(){var elem = findTextInDOM("Cellule de ","span");if(elem && !clickedOnCell){clickedOnCell=true;$(elem).click()}}
-function clickOnBestMatchup(){var bm="",bw=0;for(var i in brutesDivs){var w=parseFloat(brutesDivs.tx.text());if(w>bw){bw=w;bm=i}};cl(bm,bw,findTextInDOM(bm,"p"))}
+function clickOnBestMatchup(){var bm="",bw=0;for(var i in brutesDivs){var w=parseFloat(brutesDivs[i].tx.text());if(w>bw){bw=w;bm=i}};cl(bm,bw,findTextInDOM(bm,"p"))}
 function makeSureArenaIsRunning(){if(!arenaRunning){arena()}}
 
 function precedent(){if(!clickedOnCell){clickedOnCell=true;history.back()};}
