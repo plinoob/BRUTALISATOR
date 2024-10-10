289b34401d299c1e25329bc0155a8ade9d7bd730
Ambryal <gohieralexis@yahoo.fr>
2024-10-11 01:07:05 +0200

    Ajout des fichiers .js du dossier dist

--- war.js
+++ war.js
@@ -2616,7 +2616,7 @@ async function simulWar(){
 			var l=[]
 			for(var brute of clans[side].brutes){brutes[brute.name]=brute;l.push([brute.level,brute.name,brute])}
 			l.sort(function(a,b){return b[0]-a[0]})
-			for(var b of l){var dv=div({26:0,15:0,9:{"border-radius": "15px",display: flex,"justify-content": "space-between","width": "75%"}
+			for(var b of l){var dv=div({26:0,15:0,9:{"border-radius": "15px",display: "flex","justify-content": "space-between","width": "75%"}
 			,0:sidiv,6:{mousedown:bruteClic(b[2],side,dv)}});div({0:dv,17:b[1]});div({0:dv,17:"<b>"+b[0]+"</b>"})}
 	
 		}
