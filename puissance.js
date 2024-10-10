1831050a0366dc548b99ffe41a97afef2211b409
Ambryal <gohieralexis@yahoo.fr>
2024-10-10 20:24:38 +0200

    Ajout des fichiers .js du dossier dist

--- puissance.js
+++ puissance.js
@@ -5586,7 +5586,7 @@ function afficheur(bilan){
 	bilan=[];for(var b in persos){bilan.push(persos[b])}
 	
 	for(var b of bilan){var l=b.nom.split("$");perkType=l[2];perk=l[3];sens=l[1]=="+"
-				if(perk=="dog1"){cl(bilan)}
+				cl(perk,b)
 				var btn=$("#"+perkType+perk)
 				if(!btn.length){btn=makeAnaDiv(perkType,perk,sens)}
 				
