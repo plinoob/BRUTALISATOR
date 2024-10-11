608be15af106becb92914370a2f71acee3afebe1
Ambryal <gohieralexis@yahoo.fr>
2024-10-11 03:15:04 +0200

    Ajout des fichiers .js du dossier dist

--- war.js
+++ war.js
@@ -2588,7 +2588,7 @@ async function simulWar(){
 			
 			var sidiv = $("#sidiv"+side)
 			if(!sidiv.length){
-				sidiv=div({1:"sidiv"+side,13:300000001,4:[10,side?7:78,"",side?78:7],3:"power",26:1,15:"default",9:uni([{ "font-size":"0.821429rem"
+				sidiv=div({1:"sidiv"+side,13:300000001,4:[10,side?6:77,"",side?77:6],3:"power",26:1,15:"default",9:uni([{ "font-size":"0.821429rem"
 				,display: "flex","flex-direction": "column","justify-content": "start","align-items": "center"},
 				textBoxCSS,baseCSS,{"padding": "5px"}])})
 			}
@@ -2604,7 +2604,7 @@ async function simulWar(){
 				if(rota1.length && rota2.length){
 				simulFights({
 					fn:function(bilan,ended){stopLoading()
-						cl(bilan)
+						//cl(bilan)
 						var b=bilan[0]
 						if(b.j>0){
 						var coef=(b.v/b.j)
@@ -2634,7 +2634,8 @@ async function simulWar(){
 			var l=[]
 			for(var brute of clans[side].brutes){if(fighterUsed.includes(brute.name))continue;brutes[brute.name]=brute;l.push([brute.level,brute.name,brute])}
 			l.sort(function(a,b){return b[0]-a[0]})
-			for(var b of l){var dv=div({26:0,15:0,9:{padding:"1px 17px",margin:"1px","border-radius": "15px",display: "flex","justify-content": "space-between","width": "80%"}
+			for(var b of l){var dv=div({20:"[Clic Molette] Visiter la cellule",26:0,15:0,9:{padding:"1px 17px",margin:"1px",
+			"border-radius": "15px",display: "flex","justify-content": "space-between","width": "86%"}
 			,0:sidiv});dv.on("mousedown",bruteClic(b[2],side,dv));div({0:dv,17:b[1]});div({0:dv,17:"<b>"+b[0]+"</b>"})}
 	
 		}
