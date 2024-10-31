3ccf5d0b25308bfd8d0e76c15de14d7017b98923
Ambryal <gohieralexis@yahoo.fr>
2024-10-31 15:10:15 +0100

    Ajout des fichiers .js du dossier dist

--- color.js
+++ color.js
@@ -1236,7 +1236,8 @@ div({0:shurikenDIV,2:"img",22:SHURIKEN,1:"shuriken-image",9:{height:"133px",}})
     "font-size": "1rem",
     "line-height": "1.5",
     "letter-spacing": "0.00938em",
-}cl('SOMBRE',$("#root"),$("#root").css("background"),$("#root").css("background").includes("linear-gradient(rgb(247, 225, 183) 0%, rgb(235, 173, 112) 160px)"),$("#root").css("background").includes("linear-gradient(rgb(247"))
+}
+cl('SOMBRE',$("#root"),$("#root").css("background"),$("#root").css("background").includes("linear-gradient(rgb(247, 225, 183) 0%, rgb(235, 173, 112) 160px)"),$("#root").css("background").includes("linear-gradient(rgb(247"))
 if(SOMBRE===undefined)SOMBRE = $("#root").css("background").includes("linear-gradient(rgb(247, 225, 183) 0%, rgb(235, 173, 112) 160px)")
 if(SOMBRE){
 textBoxCSS = {color: "#E8EDE8",
